79cb529b22aa8066a08e4cf8b0ebeebc7e43dc02
Igor D.C <igor.duarte.cardoso@intel.com>
2018-08-13 18:22:32 +0100

    Fixes render.sh to be run from anywhere
    Previously one would have to be present in the repository root,
    otherwise the theme would not be located (would always be searched
    by Hugo relative to current working directory).

--- tools/render.sh
+++ tools/render.sh
@@ -3,5 +3,6 @@
 REPO_ROOT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 default_ip=$(ip a show $(route -n | grep UG | grep 0.0.0.0 | awk '{print $8}') | grep "inet "  | awk '{print $2}' | cut -d "/" -f 1)
 #default_ip="127.0.0.1"
+cd ${REPO_ROOT_DIR} # to allow ./render to be run from anywhere
 ${REPO_ROOT_DIR}/tools/hugo server --config ${REPO_ROOT_DIR}/config.yaml --renderToDisk --bind "${default_ip}" -b http://${default_ip} --noHTTPCache --disableFastRender -v
 
