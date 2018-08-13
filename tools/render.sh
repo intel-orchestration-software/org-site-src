095eaede0db630b480fea1d8a27aaf5ff7441c47
Igor D.C <igor.duarte.cardoso@intel.com>
2018-08-13 18:06:03 +0100

    Removes unneeded ifconfig command from render.sh

--- tools/render.sh
+++ tools/render.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-ifconfig
-
 REPO_ROOT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 default_ip=$(ip a show $(route -n | grep UG | grep 0.0.0.0 | awk '{print $8}') | grep "inet "  | awk '{print $2}' | cut -d "/" -f 1)
 #default_ip="127.0.0.1"
